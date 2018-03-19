028a09a6e423fd91e1343552aa6a1fde88e12684
Arthur MASSON <amasson@e2r11p18.42.fr>
2018-03-19 10:51:33 +0100

    fixed iOS cast problem

--- Firefox Shared/Extensions/SCNVector.swift
+++ Firefox Shared/Extensions/SCNVector.swift
@@ -11,9 +11,9 @@ import SceneKit
 extension SCNVector3 {
     
     static func random(inBounds bounds: (min: SCNVector3, max: SCNVector3)) -> SCNVector3 {
-        return SCNVector3(x: CGFloat.random(min: bounds.min.x, max: bounds.max.x),
-                          y: CGFloat.random(min: bounds.min.y, max: bounds.max.y),
-                          z: CGFloat.random(min: bounds.min.z, max: bounds.max.z))
+        return SCNVector3(CGFloat.random(min: CGFloat(bounds.min.x), max: CGFloat(bounds.max.x)),
+                          CGFloat.random(min: CGFloat(bounds.min.y), max: CGFloat(bounds.max.y)),
+                          CGFloat.random(min: CGFloat(bounds.min.z), max: CGFloat(bounds.max.z)))
     }
     
     func distanceTo(point: SCNVector3) -> SCNFloat {
@@ -24,6 +24,7 @@ extension SCNVector3 {
         return SCNVector3(x: lhs.x - rhs.x, y: lhs.y - rhs.y, z: lhs.z - rhs.z)
     }
 }
+
 func distanceBetweenPoints(_ a: SCNVector3, _ b: SCNVector3) -> SCNFloat {
     return sqrt((a.x - b.x) * (a.x - b.x)
         + (a.y - b.y) * (a.y - b.y)
